4152ea089b81fdce56063688b968cf2081a1f365
wimpyKid781 <71785664+wimpyKid781@users.noreply.github.com>
2020-12-07 19:47:55 -0800

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,28 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 1
+        }
+        this.chain = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.chain);
+
+    }
+    fly(){
+        this.chain.bodyA =null;
+    }
+    display(){
+        if(this.chain.bodyA){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(4);
+        stroke("turquoise");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        }
+    
+}
